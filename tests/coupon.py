a1ac82769f2f763c3cb17307eab69ce7f55aea1a
iclapcheeks <51503087+iclapcheeks@users.noreply.github.com>
2021-06-09 16:07:23 -0700

    Create coupon.py

---
+++ tests/coupon.py
@@ -0,0 +1,22 @@
+import seltix
+
+sellix = seltix.Sellix(
+    secret_key='SECRET_KEY'
+)
+
+coupon = sellix.request(
+    method='create', # What you want to do, in this case we want to create a new coupon code
+    endpoint='coupon', # The API Endpoint you are trying to access
+
+    code='Seltix is awesome',
+    discount_value=0.15,
+    max_uses=5,
+    products_bound=[
+        'seltix2021abc'
+    ]
+
+    # Passing the arguments required to create the coupon code, some are
+    # required while some are not, read more at https://developers.sellix.io
+)
+
+print(coupon['data']['uniqid']) # Getting the ID of the newly created coupon code
